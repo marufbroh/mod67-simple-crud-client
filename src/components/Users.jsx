43c64c76f0e8d9af74c2ed9cbd72798ee39387ca
marufbroh <marufmd775@gmail.com>
2023-05-09 07:31:57 +0600

    final commit crud client

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Users = () => {
-    const users = useLoaderData()
+    const LoadedUsers = useLoaderData()
     // console.log(users);
+    const [users, setUsers] = useState(LoadedUsers)
+
     const handleDelete = _id => {
         console.log(_id);
         fetch(`http://localhost:5000/users/${_id}`, {
             method: "DELETE",
-            // headers: {
-            //     "content-type": "application/json"
-            // }, 
-            // body: ''
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.deletedCount> 0){
-                alert('User successfully deleted')
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    alert('User successfully deleted');
+                    const remaining = users.filter(user => user._id !== _id);
+                    setUsers(remaining)
+                }
+            })
     }
 
     return (
@@ -27,7 +27,7 @@ const Users = () => {
             <h2>{users.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email} <button onClick={() => handleDelete(user._id)}>X</button></p>)
+                    users.map(user => <p key={user._id}>{user.name} : {user.email} <Link to={`/update/${user._id}`}>Update</Link> <button onClick={() => handleDelete(user._id)}>X</button></p>)
                 }
             </div>
         </div>
